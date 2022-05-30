f8fcd37c5cd2e447109bd25665bf28a6bb89a066
Crowdbotics <team@crowdbotics.com>
2022-05-30 12:15:05 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "gapapp_35507",
+  url: "https://gapapp_35507.botics.co",
+  api: "https://gapapp_35507.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
